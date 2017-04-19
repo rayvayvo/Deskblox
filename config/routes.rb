6a8767246e7e24df757a9586a5e690e943c34a88
Ryan Daley <rpdaley@gmail.com>
2017-04-19 20:36:03 +0000

    wireframe fixed layout complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'blocks/index'
+
   resources :users
+  resources :blocks
 
   root to: 'dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
