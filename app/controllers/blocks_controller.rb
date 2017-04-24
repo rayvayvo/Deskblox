bf4d9450f6124b36b03c41cd71e99625e5aaf5dd
Raven Ackerman <rayvayvo@gmail.com>
2017-04-24 16:11:23 +0000

    added redirects for user login state

--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -1,4 +1,10 @@
 class BlocksController < ApplicationController
+
   def index
+
+    if !current_user
+      redirect_to "/"
+    end
   end
+
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,10 @@
 class DashboardController < ApplicationController
 
 
+  def index
+    if current_user
+      redirect_to "/blocks"
+    end
+  end
+
 end
